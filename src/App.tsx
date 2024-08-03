a2a11add832465c1b8799af19cfb55ac91fa46b7
Dustine Trieu <dtrieu7679@gmail.com>
2024-08-03 13:04:28 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
